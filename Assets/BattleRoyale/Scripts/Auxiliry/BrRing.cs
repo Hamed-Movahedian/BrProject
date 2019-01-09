3813936ed4c4e435c809358fb5aef5aa77bddab7
Hamed Movahedian <movahedian.h@gmail.com>
2019-01-09 19:56:53 +0330

    no message

---
+++ Assets/BattleRoyale/Scripts/Auxiliry/BrRing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrRing : MonoBehaviour
+{
+    public Transform Outter;
+    public Transform Inner;
+
+    public float radious=3;
+    public float thickness=.2f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnValidate()
+    {
+        setup();
+    }
+
+    private void setup()
+    {
+        Inner.localScale = Vector3.one * radious;
+        Outter.localScale = Vector3.one * (radious + thickness);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/BattleRoyale/Scripts/Singletons/BrKillZone.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class BrKillZone : MonoBehaviourPunCallbacks
+{
+    public BrRing currCenter;
+    public BrRing targetCenter;
+
+    public float TimeToNextRing=15;
+    public float ChangeTime = 10;
+
+    private float _shrinkTime=-1;
+	void Start ()
+	{
+        gameObject.SetActive(false);
+	    if (PhotonNetwork.IsMasterClient)
+	        CreateNextCircle();
+	}
+
+    private void CreateNextCircle()
+    {
+        var nextR = currCenter.radious * .7f;
+        Vector3 nextCenter = Random.insideUnitCircle * (currCenter.radious - nextR);
+        nextCenter = new Vector3(nextCenter.x, 0, nextCenter.y) + currCenter.transform.localPosition;
+        photonView.RPC("NewCircle", RpcTarget.AllViaServer, nextCenter, nextR);
+    }
+
+    [PunRPC]
+    public void NewCircle(Vector3 center, float radious)
+    {
+        gameObject.SetActive(true);
+        targetCenter.transform.localPosition = center;
+        targetCenter.radious = radious;
+        _shrinkTime = TimeToNextRing + ChangeTime;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (_shrinkTime < 0)
+            return;
+        _shrinkTime -= Time.deltaTime;
+
+        if (_shrinkTime <= 0)
+        {
+            currCenter.transform.localPosition = targetCenter.transform.localPosition;
+            currCenter.radious = targetCenter.radious;
+
+            if (PhotonNetwork.IsMasterClient)
+                CreateNextCircle();
+
+        }
+
+        if (_shrinkTime <= ChangeTime)
+        {
+            currCenter.transform.localPosition = Vector3.Lerp(
+                currCenter.transform.localPosition,
+                targetCenter.transform.localPosition,
+                _shrinkTime / ChangeTime);
+
+            currCenter.radious = Mathf.Lerp(
+                currCenter.radious,
+                targetCenter.radious,
+                _shrinkTime / ChangeTime);
+        }
+    }
+}
