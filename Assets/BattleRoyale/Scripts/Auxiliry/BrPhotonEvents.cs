95587c1804c9eaf5e3c617601b2585b5e76752ee
DESKTOP-SM4SNDC\Hamed <Movahedian.h@gmail.com>
2019-01-19 20:30:51 +0330

    no message

---
+++ Assets/BattleRoyale/Scripts/Auxiliry/BrPhotonEvents.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BrPhotonEvents : MonoBehaviour
+{
+    public UnityEvent OnMaster;
+    public UnityEvent OnNotMaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+            OnMaster.Invoke();
+        else
+            OnNotMaster.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
