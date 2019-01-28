bac52a462b1f04df4e7fa9cd1dfcd6f18453c60c
HoseinNajafi <najafi.h.ir@gmail.com>
2019-01-28 23:52:23 +0330

    no message

---
+++ Assets/ThirdParties/Editor/SetArabicTextExample.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using ArabicSupport;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class PersianTextWindow : EditorWindow
+{
+    public string Text;
+    public string FixedText;
+
+    
+    [MenuItem("Window/Persian Text")]
+    static void CreateWizard()
+    {
+        PersianTextWindow window = (PersianTextWindow)EditorWindow.GetWindow(typeof(PersianTextWindow));
+        window.titleContent=new GUIContent("Persian Text");
+        window.Show();
+
+    }
+
+    void OnGUI()
+    {
+        Text = EditorGUILayout.TextField("Text ", Text);
+        if(!string.IsNullOrEmpty(Text))
+        EditorGUILayout.LabelField("Fix text ",PersianFixer.Fix(Text));
+        if (GUILayout.Button("Copy"))
+            EditorGUIUtility.systemCopyBuffer = PersianFixer.Fix(Text);
+
+    }
+
+}
