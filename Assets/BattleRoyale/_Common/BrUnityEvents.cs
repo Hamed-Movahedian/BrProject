2d274032e7e5c149f7241654e4b10a404f8e696b
HoseinNajafi <najafi.h.ir@gmail.com>
2019-02-02 23:58:06 +0330

    no message

---
+++ Assets/BattleRoyale/_Common/BrUnityEvents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class UnityEventsString : UnityEvent<string>
+{
+}
+public class UnityEventsFloat : UnityEvent<float>
+{
+}
+public class UnityEventsVector3 : UnityEvent<Vector3>
+{
+}
+
