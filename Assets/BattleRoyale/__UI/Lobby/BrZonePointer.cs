a0ce7ab7878a7854573dc99686699bf68e64a6af
HoseinNajafi <najafi.h.ir@gmail.com>
2019-02-04 16:55:06 +0330

    no message

---
+++ Assets/BattleRoyale/__UI/Lobby/BrZonePointer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class BrZonePointer : MonoBehaviour
+{
+    public string ZoneName;
+    public Text ZoneText;
+
+    public UnityEvent Show, Hide;
+    private List<BrZonePointer> _zones;
+    private bool _active = false;
+    void Start()
+    {
+        _zones= transform.parent.GetComponentsInChildren<BrZonePointer>().ToList();
+        _zones.Remove(this);
+        GetComponent<Button>().onClick.AddListener(() => Clicked());
+        ZoneText.text = ZoneName;
+    }
+
+    private void Clicked()
+    {
+        _zones.ForEach(z=>z.UnClicked());
+        if (_active)
+            return;
+        
+        Show.Invoke();
+        _active = true;
+    }
+
+    private void UnClicked()
+    {
+        if(!_active) 
+            return;
+        
+        Hide.Invoke();
+        _active = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
