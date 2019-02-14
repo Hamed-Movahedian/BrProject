5b95a2a326a6871da116ed105c106265cd69b25a
DESKTOP-SM4SNDC\Hamed <Movahedian.h@gmail.com>
2019-02-14 09:15:08 +0330

    no message

---
+++ Assets/BattleRoyale/__Controllers/Air Drop/AirDropPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirDropPickup : MonoBehaviour
+{
+    [SerializeField]
+    private float radious = 3;
+
+    [SerializeField] private Color color=Color.red;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position,radious);
+    }
+
+}
