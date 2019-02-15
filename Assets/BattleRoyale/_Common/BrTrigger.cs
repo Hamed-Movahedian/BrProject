7c11340e46fd2b69f34f9a98f03187a7d133468d
HoseinNajafi <najafi.h.ir@gmail.com>
2019-02-15 20:16:32 +0330

    no message

---
+++ Assets/BattleRoyale/_Common/BrTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BrTrigger : MonoBehaviour
+{
+    public UnityEvent OnEnter, OnExit;
+    
+    private BrPlayerTracker tracker;
+
+    private void Start()
+    {
+        tracker = BrPlayerTracker.Instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsActivePlayer(other)) OnEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (IsActivePlayer(other)) OnExit.Invoke();
+    }
+
+    private bool IsActivePlayer(Collider other)
+    {
+        return true;
+    }
+}
