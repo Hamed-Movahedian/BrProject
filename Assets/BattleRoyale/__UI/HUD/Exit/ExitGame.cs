32e9734ce28ab9101ac6415d33cc9c8972f547c8
HoseinNajafi <najafi.h.ir@gmail.com>
2019-02-17 23:05:24 +0330

    no message

---
+++ Assets/BattleRoyale/__UI/HUD/Exit/ExitGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    public UnityEvent OnInputEscape;
+
+    public void ExitMach()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            OnInputEscape.Invoke();
+        }
+    }
+}
