e381417fc1c49f9abeedb7078d6a2f0c358fd04a
HoseinNajafi <najafi.h.ir@gmail.com>
2019-05-11 20:12:15 +0430

    no message

---
+++ Assets/BattleRoyale/_Emotes/EmoteData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Emote Data", menuName = "BattleRoyal/Emote Data", order = 5)]
+public class EmoteData : ScriptableObject
+{
+    [HideInInspector]
+    public int ID = -1;
+
+    public string Name;
+
+    public Texture2D Icon;
+
+    public bool HasByDefault = false;
+}
